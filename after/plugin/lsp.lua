6843076abaf31c020cdd03f4a775e73993f59d0c
Vivien Rindisbacher <56514534+vrindisbacher@users.noreply.github.com>
2023-09-03 10:52:48 -0400

    Update lsp.lua

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -37,6 +37,26 @@ lsp.set_preferences({
     }
 })
 
+
+        error = 'E',
+        warn = 'W',
+        hint = 'H',
+        info = 'I'
+    }
+})
+
+lsp.on_attach(function(client, bufnr)
+  local opts = {buffer = bufnr, remap = false}
+
+  vim.keymap.set("n", "gt", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "hov", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+  vim.keymap.set("n", "gtn", function() vim.diagnostic.goto_next() end, opts)
+  vim.keymap.set("n", "gtp", function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+end)
 lsp.setup()
 
 vim.diagnostic.config({
