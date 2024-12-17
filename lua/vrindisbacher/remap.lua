d585da41a75926172479770ee85687d274382343
vrindisbacher <vrindisbacher77@gmail.com>
2024-12-17 08:49:55 -0800

    Add awesome terminal stuff

--- lua/vrindisbacher/remap.lua
+++ lua/vrindisbacher/remap.lua
@@ -2,3 +2,24 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "C-d", "C-dzz")
 vim.keymap.set("n", "C-d", "C-dzz")
+
+vim.keymap.set("n", "<leader>t", function()
+    for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+        local buf = vim.api.nvim_win_get_buf(win)
+        if vim.bo[buf].buftype == "terminal" then
+            vim.api.nvim_set_current_win(win)
+	    vim.cmd("startinsert")
+            return
+        end
+    end
+    vim.cmd.vnew()
+    vim.cmd.term()
+    vim.cmd.wincmd("J")
+    vim.api.nvim_win_set_height(0, 10)
+    vim.cmd("startinsert")
+end)
+
+vim.keymap.set('t', '<leader>t', function()
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-\\><C-n>", true, false, true), "n", false)
+    vim.cmd.wincmd("k")
+end)
